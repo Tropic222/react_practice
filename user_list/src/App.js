f3a029954c725d77109f7d193adfc3654c7b4fed
Tropic222 <deputat1120@gmail.com>
2024-09-14 12:43:28 +0300

    displayed a static list

--- user_list/src/App.js
+++ user_list/src/App.js
@@ -1,13 +1,27 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { Users } from './components/Users'
 import './index.scss'
 
 // Тут список пользователей: https://reqres.in/api/users
 
 function App() {
+  const [users, setUsers] = useState([])
+  const [isLoading, setLoading] = useState(true)
+  useEffect(() => {
+    fetch('https://reqres.in/api/users')
+
+    .then((res) => res.json())
+    .then((json) => {
+      setUsers(json.data)
+    })
+    .catch((err) => {
+      console.warn(err)
+      alert('ошиибка')
+    }).finally(() => setLoading(false))
+  }, [] )
   return (
     <div className="App">
-      <Users />
+      <Users items={users} isLoading={isLoading}/>
       {/* <Success /> */}
     </div>
   );
