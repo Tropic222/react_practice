359a244094624f05c95bbe52412412303ab064b3
Tropic222 <deputat1120@gmail.com>
2024-09-18 14:33:25 +0300

    added conversion when entering amount

--- converter/src/App.js
+++ converter/src/App.js
@@ -27,10 +27,15 @@ function App() {
   }, [])
 
   const onChangeFromPrice = (value) => {
+    const price = value / rates[fromCurrency]; 
+    const result = price * rates[toCurrency]
+    setToPrice(result)
     setFromPrice(value)
   }
 
   const onChangeToPrice = (value) => {
+    const result = (rates[fromCurrency] / rates[toCurrency] )* value;
+    setFromPrice(result)
     setToPrice(value)
   }
   return (
